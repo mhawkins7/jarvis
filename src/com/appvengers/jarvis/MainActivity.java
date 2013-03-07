e3de4b868b7269f9edcfd156ac036b3b37f0b831
Matt Hawkins <trumpet580@gmail.com>
2013-03-07 00:32:47 -0500

    now has home screen with buttons to daily schedule and task list

--- src/com/appvengers/jarvis/MainActivity.java
+++ src/com/appvengers/jarvis/MainActivity.java
@@ -2,11 +2,17 @@ package com.appvengers.jarvis;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	
+	public final static String EXTRA_MESSAGE = "com.appvengers.jarvis.MESSAGE"; 
 
 	@Override
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -17,6 +23,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	/**
+	 * Called when user clicks send button
+	 * @param view
+	 */
+	public void displaySchedule(View view){
+		
+		Intent intent = new Intent(this, DisplayScheduleActivity.class);
+		startActivity(intent);
+	}
+	
+	public void displayTasks(View view){
+		
+		Intent intent = new Intent(this, DisplayTasksActivity.class);
+		startActivity(intent);
 	}
 
 }
